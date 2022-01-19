9323d316dc3b212231d67831ce9ce976bf023627
Vinicius Rossado <vinirossado@gmail.com>
2022-01-19 12:07:33 -0300

    arrumando mapeamento de City

--- models/city.model.go
+++ models/city.model.go
@@ -1,11 +1,17 @@
 package models
 
 type City struct {
-	ID        string `json:"id"`
-	IDCountry string `json:"idCountry"`
-	Name      string `json:"name"`
-	Latitude  string `json:"latitude"`
-	Longitude string `json:"longitude"`
+	ID          string        `json:"id"`
+	IDCountry   string        `json:"idCountry"`
+	Name        string        `json:"name"`
+	Latitude    string        `json:"latitude"`
+	Longitude   string        `json:"longitude"`
+	ImageUrl    string        `json:"imageUrl"`
+	Blurhash    string        `json:"blurhash"`
+	Description interface{}   `json:"description"`
+	Country     Country       `json:"country"`
+	Images      []interface{} `json:"images"`
+	Attractions []interface{} `json:"attractions"`
 }
 
 // type CityRepository interface {
